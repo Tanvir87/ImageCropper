cd295cf39e19a8944a5f7c6fc48ad8bef237ad05
Ismith Pasha <ismithpasha@gmail.com>
2019-10-20 13:13:53 +0600

    1. With Base64 Image Encode and Decode.

--- app/src/main/java/com/smartlocation/imagecropper/MainActivity.java
+++ app/src/main/java/com/smartlocation/imagecropper/MainActivity.java
@@ -6,8 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
@@ -16,6 +19,10 @@ import android.widget.Toast;
 
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
     ImageButton btBrowse, btReset;
@@ -64,22 +71,47 @@ public class MainActivity extends AppCompatActivity {
         if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
             CropImage.ActivityResult result = CropImage.getActivityResult(data);
             if (resultCode == RESULT_OK) {
-                imageView.setImageURI(result.getUri());
-                Log.d("AAAAA",String.valueOf(result.getUri()));
+                //imageView.setImageURI(result.getUri());
+                Log.d("AAAAA", String.valueOf(result.getUri()));
+                final InputStream imageStream;
+                try {
+                    imageStream = getContentResolver().openInputStream(result.getUri());
+                    final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                    String encodedImage = encodeImage(selectedImage);
+                    Log.d("AAAAA", encodedImage);
+                    imageView.setImageBitmap(decodeImage(encodedImage));
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
                 Toast.makeText(this, "Image Update Successfully !!!", Toast.LENGTH_SHORT).show();
             }
         }
     }
 
     private void startCrop(Uri imageuri) {
-       CropImage.activity(imageuri)
-               .setGuidelines(CropImageView.Guidelines.ON)
-               .setAllowFlipping(true)
-               .setAllowRotation(true)
-               .setAspectRatio(400,400)
-               .setMultiTouchEnabled(true)
-               .start(this);
+        CropImage.activity(imageuri)
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .setAllowFlipping(true)
+                .setAllowRotation(true)
+                .setAspectRatio(400, 400)
+                .setMultiTouchEnabled(true)
+                .start(this);
     }
+
+    private String encodeImage(Bitmap bm) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bm.compress(Bitmap.CompressFormat.JPEG, 60, baos);
+        byte[] b = baos.toByteArray();
+        String encImage = Base64.encodeToString(b, Base64.DEFAULT);
+        return encImage;
+    }
+
+    private Bitmap decodeImage(String encodedImage) {
+        byte[] decodedString = Base64.decode(encodedImage, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+    }
+
+
 }
 
 
